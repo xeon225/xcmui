b17ec622200b92e4a3f669cdda7ab22c1b7d4aea
System Administrator <root@xeoniMacProMax.local>
2022-05-24 16:59:49 +0800

    badge 模块

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,15 @@
 <template>
 <div class="paddingh20">
-  <Color class="paddingv30 borderb"></Color>
-  <Font class="paddingv30 borderb"></Font>
+  <div class="pos-f">
+    <div class="flex-container nav-list">
+      <a class="bg-white" href="#color">颜色</a>
+      <a class="bg-white" href="#font">文字段落</a>
+      <a class="bg-white" href="#badge">标识</a>
+    </div>
+  </div>
+  <Color id="color" class="paddingv30 borderb"></Color>
+  <Font id="font" class="paddingv30 borderb"></Font>
+  <Badge id="badge" class="paddingv30 borderb"></Badge>
 </div>
 
 </template>
@@ -9,11 +17,13 @@
 <script>
 import Color from '@/components/page/color.vue'
 import Font from '@/components/page/font.vue'
+import Badge from '@/components/page/badge.vue'
 export default {
   name: 'index',
   components: {
     Color,
-    Font
+    Font,
+    Badge
   },
   data () {
     return {
@@ -30,5 +40,10 @@ h1{
 </style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.nav-list{
+  a{
+    padding:10px;
+    color:#333;
+  }
+}
 </style>
---
+++ src/components/page/badge.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <h1>标识</h1>
+  <div class="text-dark fs-10">标识颜色/标识状态/标识组</div>
+  <div class="fs-18 margint20">标识颜色</div>
+  <div class="badge_list">
+    <div class="flex-container-col center margint10" v-for="(item, index) in colorText" :key="index">
+      <div class="badge" :class="'badge-'+item" v-text="item"></div>
+      <div class="margint5 fs-12" v-text="'badge-'+item"></div>
+    </div>
+  </div>
+  <div class="fs-18 margint20">标识状态</div>
+  <div class="badge_list margint20" v-for="(itemT, indexT) in badgeType" :key="indexT">
+    <div class="flex-container-col center margint10" v-for="(item, index) in colorText" :key="index">
+      <div class="badge" :class="'badge-'+item+' badge-'+itemT" v-text="itemT"></div>
+      <div class="margint10 fs-12" v-text="'badge-'+itemT"></div>
+    </div>
+  </div>
+  <div class="fs-18 margint20">标识组</div>
+  <div>
+    <div class="badge-group margint20" v-for="(itemT, indexT) in badgeTrem" :key="indexT">
+      <div class="badge badge-red" :class="'badge-'+itemT" v-for="(item, index) in 9" :key="index" v-text="item"></div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'color',
+  data () {
+    return {
+      colorText: ['black', 'white', 'green', 'orange', 'red', 'yellow', 'blue', 'coffee', 'purple'],
+      badgeType: ['pill', 'reverse', 'round', 'radius', 'flag'],
+      badgeTrem: ['', 'reverse', 'round']
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.badge_list{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  .badge-white{
+    color:#666;
+    border:1px solid #ccc;
+  }
+}
+</style>
